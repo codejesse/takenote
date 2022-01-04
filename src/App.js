499586f1870f78e84883a68a7fc0c4d1a6cd3d17
Doris Ngere <jessebeke10@gmail.com>
2022-01-04 04:24:33 +0100

    takenote commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import { useRef, useState } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import "./index.css";
+// import background from './background.svg'
+
+
+function App() {
+  const [text, setText] = useState('')
+  const { transcript, resetTranscript } = useSpeechRecognition();
+  const [isListening, setIsListening] = useState(false);
+  const microphoneRef = useRef(null);
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return (
+      <div className="mircophone-container">
+        Browser is not Support Speech Recognition.
+      </div>
+    );
+  }
+  const handleListing = () => {
+    setIsListening(true);
+    microphoneRef.current.classList.add("listening");
+    SpeechRecognition.startListening({
+      continuous: true,
+    });
+  };
+  const stopHandle = () => {
+    setIsListening(false);
+    microphoneRef.current.classList.remove("listening");
+    SpeechRecognition.stopListening();
+  };
+  const handleEdit = () => {
+    stopHandle();
+    resetTranscript();
+  };
+
+  // const handleClear = () => {
+  //   setText('')
+  // };
+
+  return (
+    <div className="microphone-wrapper">
+      <div className="mircophone-container">
+        <div
+          className="microphone-icon-container"
+          ref={microphoneRef}
+          onClick={handleListing}
+        >
+          <img src="https://img.icons8.com/color-glass/48/000000/microphone.png" alt="mic"/>
+        </div>
+        <div className="microphone-status">
+          {isListening ? "Listening........." : "Click and say anything"}
+        </div>
+        {isListening && (
+          <button className="microphone-stop btn" onClick={stopHandle}>
+            Stop
+          </button>
+        )}
+      </div>
+      {(
+        <div className="microphone-result-container">
+          <textarea cols="30" rows="10" spellCheck="false" value={transcript || text}  onChange={e => setText(e.target.value)}/>
+          <button className="microphone-reset btn" onClick={handleEdit}>
+            Edit
+          </button>
+          {/* <button className="microphone-reset btn" onClick={handleClear}>
+            clear
+          </button> */}
+        </div>
+      )}
+    </div>
+  );
+}
+export default App;
